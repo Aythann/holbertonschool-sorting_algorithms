1a17fb01ec1ff8e662f3f1dfa1fa48210764e73b
Aythann <aythancristovao@gmail.com>
2025-12-12 15:46:40 +0100

    removed funtion prototype for 3-quick_sort.c file

--- sort.h
+++ sort.h
@@ -21,7 +21,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quick_sort_rec(int *array, int little, int bigger, size_t size);
 
 /* file test */
 void print_array(const int *array, size_t size);
